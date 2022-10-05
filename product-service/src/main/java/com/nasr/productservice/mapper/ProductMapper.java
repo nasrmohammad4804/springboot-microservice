1fd0e07a0794188cb7b3328264ab56562e2ab5b2
mohammad <nasrmohammad4804@gmail.com>
2022-10-05 20:31:13 +0330

    change ProductMapper

--- product-service/src/main/java/com/nasr/productservice/mapper/ProductMapper.java
+++ product-service/src/main/java/com/nasr/productservice/mapper/ProductMapper.java
@@ -2,12 +2,12 @@ package com.nasr.productservice.mapper;
 
 import com.nasr.productservice.base.mapper.BaseMapper;
 import com.nasr.productservice.domain.Product;
-import com.nasr.productservice.dto.request.ProductRequestDto;
-import com.nasr.productservice.dto.response.ProductResponseDto;
+import com.nasr.productservice.dto.request.ProductRequest;
+import com.nasr.productservice.dto.response.ProductResponse;
 import org.mapstruct.Mapper;
 
 
 @Mapper
-public interface ProductMapper extends BaseMapper<Product, ProductResponseDto, ProductRequestDto> {
+public interface ProductMapper extends BaseMapper<Product, ProductResponse, ProductRequest> {
 
 }
