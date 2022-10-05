99e9a7b5d736894fd33caec81a4ce486e7796106
mohammad <nasrmohammad4804@gmail.com>
2022-10-05 21:03:24 +0330

    add constant

---
+++ order-service/src/main/java/com/nasr/orderservice/constant/ConstantField.java
@@ -0,0 +1,8 @@
+package com.nasr.orderservice.constant;
+
+
+public class ConstantField {
+
+    public static final String ORDER_HANDLER_GROUP_NAME="order-handler";
+    public static final int ORDER_HANDLER_DEFAULT_HOUR=1;
+}
