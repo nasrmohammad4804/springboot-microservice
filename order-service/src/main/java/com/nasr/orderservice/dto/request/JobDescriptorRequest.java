063fe5a50e516667928f95ce964e2952da28f83a
mohammad <nasrmohammad4804@gmail.com>
2022-10-05 20:56:19 +0330

    add RevertProductRequest for revert product to stock

---
+++ order-service/src/main/java/com/nasr/orderservice/dto/request/JobDescriptorRequest.java
@@ -0,0 +1,20 @@
+package com.nasr.orderservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobDescriptorRequest {
+
+    private String name;
+    private long orderId;
+    private List<TriggerDescriptorRequest> triggers = new ArrayList<>();
+}
