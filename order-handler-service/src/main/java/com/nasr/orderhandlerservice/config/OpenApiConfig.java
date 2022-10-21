61cb3b9d98e1baa962473c275838e2d1329af221
mohammad <nasrmohammad4804@gmail.com>
2022-10-21 20:58:25 +0330

    add order handler configuration

---
+++ order-handler-service/src/main/java/com/nasr/orderhandlerservice/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.nasr.orderhandlerservice.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer")
+public class OpenApiConfig {
+}
---
+++ order-handler-service/src/main/java/com/nasr/orderhandlerservice/config/ResourceServerConfig.java
@@ -0,0 +1,27 @@
+package com.nasr.orderhandlerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        http.cors(ServerHttpSecurity.CorsSpec::disable)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange()
+                .pathMatchers("/v3/api-docs/**", "/webjars/**")
+                .permitAll()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
+}
