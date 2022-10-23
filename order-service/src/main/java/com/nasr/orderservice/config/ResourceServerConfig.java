fef3ea992fb4dc43e68b67ecc483459a5ddb7aac
mohammad <nasrmohammad4804@gmail.com>
2022-10-23 19:04:52 +0330

    configuration stateless session management

--- order-service/src/main/java/com/nasr/orderservice/config/ResourceServerConfig.java
+++ order-service/src/main/java/com/nasr/orderservice/config/ResourceServerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -50,6 +51,8 @@ public class ResourceServerConfig {
                 .authenticated()
                 .and()
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt().jwtAuthenticationConverter(converter()));
+
+        http.securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
 
         return http.build();
     }
